5b46c28aa20f0f377d20a05d46663162ab486bbb
nilzxq <2013358717@qq.com>
2018-07-27 15:49:22 +0800

    hanoiProblem2

--- src/book_2018_07_24_stack/HanoiProblem.java
+++ src/book_2018_07_24_stack/HanoiProblem.java
@@ -1,4 +1,7 @@
 package book_2018_07_24_stack;
+
+import java.util.Stack;
+
 /** 
 * @author nilzxq
 * @version 2018年7月27日 上午11:14:41 
@@ -41,5 +44,41 @@ public class HanoiProblem {
 			int part5=process(num-1, left, mid, right, from, to);
 			return part1+part2+part3+part4+part5;
 		}
+	}
+	
+	public enum Action{
+		No,LToM,MToL,MToR,RToM
+	}
+	public int hanoiProblem2(int num,String left,String mid,String right) {
+		Stack<Integer> lS=new Stack<Integer>();
+		Stack<Integer> mS=new Stack<Integer>();
+		Stack<Integer> rS=new Stack<Integer>();
+		lS.push(Integer.MAX_VALUE);
+		mS.push(Integer.MAX_VALUE);
+		rS.push(Integer.MAX_VALUE);
+		for(int i=num;i>0;i--) {
+			lS.push(i);
+		}
+		Action[] record= {Action.No};
+		int step=0;
+		while(rS.size()!=num+1) {
+			step+=fStackTotStack(record,Action.MToL,Action.LToM,lS,mS,left,mid);
+			step+=fStackTotStack(record,Action.LToM,Action.MToL,mS,lS,mid,left);
+			step+=fStackTotStack(record, Action.RToM, Action.MToR, mS, rS, mid, right);
+			step+=fStackTotStack(record, Action.MToR,Action.RToM, rS,mS,right,mid);
+		}
+		return step;
+	}
+	
+	public static int fStackTotStack(Action[] record,Action preNoAct,Action nowAct,Stack<Integer> fStack,
+			Stack<Integer> tStack,String from,String to) {
+		if(record[0]!=preNoAct&&fStack.peek()<tStack.peek()) {
+			tStack.push(fStack.pop());
+			System.out.println("Move "+tStack.peek()+" from"+from+" to"+to);
+			record[0]=nowAct;
+			return 1;
+		}
+		return 0;
+		
 	}
 }
