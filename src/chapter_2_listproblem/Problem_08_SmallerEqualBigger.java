8caae7b9ea38a3fa779250aa86d2c6ff3c3dc954
nilzxq <2013358717@qq.com>
2018-09-10 11:35:05 +0800

    update chapter2 Problem_08 2

--- src/chapter_2_listproblem/Problem_08_SmallerEqualBigger.java
+++ src/chapter_2_listproblem/Problem_08_SmallerEqualBigger.java
@@ -72,6 +72,57 @@ public class Problem_08_SmallerEqualBigger {
 		System.out.println();
 	}
 	
+	public static Node listPartition2(Node head,int pivot) {
+		Node sH=null;//小的头
+		Node sT=null;//小的尾
+		Node eH=null;//相等的头
+		Node eT=null;//相等的尾
+		Node bH=null;//大的头
+		Node bT=null;//大的尾
+		Node next=null;//保存下一个节点
+		//所有节点分进三个链表中
+		while(head!=null) {
+			next=head.next;
+			head.next=null;
+			if(head.value<pivot) {
+				if(sH==null) {
+					sH=head;
+					sT=head;
+				}else {
+					sT.next=head;
+					sT=head;
+				}
+			}else if(head.value==pivot) {
+				if(eH==null) {
+					eH=head;
+					sH=head;
+				}else {
+					eT.next=head;
+					eT=head;
+				}
+			}else{
+				if(bH==null) {
+					bH=head;
+					bT=head;
+				}else {
+					bT.next=head;
+					bT=head;
+				}
+			}
+			head=next;
+		}
+		//小的和相等的重新连接
+		if(sT!=null) {
+			sT.next=eH;
+			eT=eT==null?sT:eT;
+		}
+		//所有的重新连接
+		if(eT!=null) {
+			eT.next=bH;
+		}
+		return sH!=null?sH:eH!=null?eH:bH;
+	}
+	
 	public static void main(String[] args) {
 		Node head1=new Node(7);
 		head1.next=new Node(9);
@@ -81,7 +132,8 @@ public class Problem_08_SmallerEqualBigger {
 		head1.next.next.next.next.next=new Node(2);
 		head1.next.next.next.next.next.next=new Node(5);
 		printLinkedList(head1);
-		head1=listPartition1(head1, 4);
+		//head1=listPartition1(head1, 4);
+		head1=listPartition2(head1, 4);
 		printLinkedList(head1);
 	}
 }
