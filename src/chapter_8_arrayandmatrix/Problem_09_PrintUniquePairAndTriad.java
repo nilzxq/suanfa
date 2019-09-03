5f253201090be9f47cd5cf87dd73326b6bbe5380
nilzxq <2013358717@qq.com>
2019-09-03 19:14:58 +0800

    Problem_09_PrintUniquePairAndTriad

---
+++ src/chapter_8_arrayandmatrix/Problem_09_PrintUniquePairAndTriad.java
@@ -0,0 +1,76 @@
+package chapter_8_arrayandmatrix;
+/** 
+* @author nilzxq
+* @version 2019年9月3日 下午6:46:12 
+*类说明 
+*/
+public class Problem_09_PrintUniquePairAndTriad {
+
+	public static void printUniquePair(int[] arr,int K) {
+		if(arr==null||arr.length<2) {
+			return;
+		}
+		int left=0;
+		int right=arr.length-1;
+		while(left<right) {
+			if(arr[left]+arr[right]<K) {
+				left++;
+			}else if(arr[left]+arr[right]>K) {
+				right--;
+			}else {
+				if(left==0||arr[left]!=arr[left-1]) {
+					System.out.println(arr[left]+","+arr[right]);
+				}
+				left++;
+				right--;
+			}
+		}
+	}
+	
+	public static void printUniqueTriad(int[] arr, int k) {
+		if (arr == null || arr.length < 3) {
+			return;
+		}
+		for (int i = 0; i < arr.length - 2; i++) {
+			if (i == 0 || arr[i] != arr[i - 1]) {
+				printRest(arr, i, i + 1, arr.length - 1, k - arr[i]);
+			}
+		}
+	}
+
+	public static void printRest(int[] arr, int f, int l, int r, int k) {
+		while (l < r) {
+			if (arr[l] + arr[r] < k) {
+				l++;
+			} else if (arr[l] + arr[r] > k) {
+				r--;
+			} else {
+				if (l == f + 1 || arr[l - 1] != arr[l]) {
+					System.out.println(arr[f] + "," + arr[l] + "," + arr[r]);
+				}
+				l++;
+				r--;
+			}
+		}
+	}
+
+	
+	public static void printArray(int[] arr) {
+		for (int i = 0; i != arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int sum = 10;
+		int[] arr1 = { -8, -4, -3, 0, 1, 2, 4, 5, 8, 9 };
+		printArray(arr1);
+		System.out.println("====");
+		printUniquePair(arr1, sum);
+		System.out.println("====");
+		printUniqueTriad(arr1, sum);
+
+	}
+
+}
