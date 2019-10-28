6a83110efb6743b49f4cd20c5f24445f2b70d856
nilzxq <2013358717@qq.com>
2019-10-28 11:19:49 +0800

    update sort Problem_06_Array_Intersect

--- src/leetcode/Problem_06_Array_Intersect.java
+++ src/leetcode/Problem_06_Array_Intersect.java
@@ -37,12 +37,44 @@ public class Problem_06_Array_Intersect {
 		return result;
 	}
 	
+	//如果给定的数组已经排好序呢？你将如何优化你的算法？
+	
+	public static int[] intersect2(int nums1[],int nums2[]) {
+		int cur1=0,cur2=0;
+		List<Integer> tempArray=new ArrayList<>();
+		while(cur1<nums1.length&&cur2<nums2.length) {
+			int num1=nums1[cur1];
+			int num2=nums2[cur2];
+			if(num1==num2) {
+				tempArray.add(num1);
+				cur1++;
+				cur2++;
+			}else if(num1>num2) {
+				cur2++;
+			}else {
+				cur1++;
+			}
+		}
+		int[] result=new int[tempArray.size()];
+		for(int i=0;i<result.length;i++) {
+			result[i]=tempArray.get(i);
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		int[] nums1= {1, 2, 2, 1};
 		int[] nums2= {2,2};
-		int[] result=intersect(nums1,nums2);
-		for(int i=0;i<result.length;i++) {
-			System.out.print(result[i]+" ");
+		int[] nums3= {1,1,2, 2,};
+		int[] nums4= {2,2};
+		int[] result1=intersect(nums1,nums2);
+		int[] result2=intersect2(nums3,nums4);
+		for(int i=0;i<result1.length;i++) {
+			System.out.print(result1[i]+" ");
+		}
+		System.out.println();
+		for(int i=0;i<result2.length;i++) {
+			System.out.print(result2[i]+" ");
 		}
 	}
 }
