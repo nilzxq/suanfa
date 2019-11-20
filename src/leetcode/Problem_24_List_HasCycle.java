1b678d7c7e89a0dadb5b9c1ecb1538d4ff299bb5
nilzxq <2013358717@qq.com>
2019-11-20 19:19:57 +0800

    Problem_24_List_HasCycle

---
+++ src/leetcode/Problem_24_List_HasCycle.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/** 
+* @author nilzxq
+* @version 2019年11月20日 下午6:58:01 
+*类说明 
+*/
+public class Problem_24_List_HasCycle {
+	
+	public static class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int x){
+			val=x;
+			next=null;
+		}
+	}
+
+	public static boolean hasCycle(ListNode head) {
+		if(head==null||head.next==null) {
+			return false;
+		}
+		ListNode slow=head;
+		ListNode fast=head;
+		while(fast!=null&&fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next.next;
+			if(slow==fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		ListNode head=new ListNode(4);
+		head.next=new ListNode(5);
+		head.next.next=new ListNode(1);
+		head.next.next.next=new ListNode(9);
+		head.next.next.next=head;
+		System.out.println(hasCycle(head));
+	}
+}
