8885c9512e0e68e6de9c4dc6c59d677f444e54d3
nilzxq <2013358717@qq.com>
2020-02-11 18:20:06 +0800

    Problem_13_String_FirstUniqChar update

--- src/leetcode/Problem_13_String_FirstUniqChar.java
+++ src/leetcode/Problem_13_String_FirstUniqChar.java
@@ -1,4 +1,7 @@
 package leetcode;
+
+import java.util.HashMap;
+
 /** 
 * @author nilzxq
 * @version 2019年11月6日 下午7:27:47 
@@ -21,10 +24,25 @@ public class Problem_13_String_FirstUniqChar {
 		return -1;
 	}
 	
+	//leetcode 哈希表专题 更巧妙解法 且使用了Java8 中新增方法
+	public static int firstUniqChar2(String s) {
+		HashMap<Character,Integer> count=new HashMap<Character,Integer>();
+		int n=s.length();
+		for(int i=0;i<n;i++) {
+			char c=s.charAt(i);
+			count.put(c,count.getOrDefault(c,0)+1);
+		}
+		for(int i=0;i<n;i++) {
+			if(count.get(s.charAt(i))==1)
+				return i;
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		String s1="leetcode";
 		String s2="loveleetcode";
 		System.out.println(firstUniqChar(s1));
 		System.out.println(firstUniqChar(s2));
+		System.out.println(firstUniqChar2(s1));
 	}
 }
