7ba308f3958cda4eb831fbf469b1c344718b2a13
nilzxq <2013358717@qq.com>
2020-06-28 22:41:40 +0800

    DP

---
+++ src/leetcode/dp/MaxProfit.java
@@ -0,0 +1,21 @@
+package leetcode.dp;
+
+/**
+ * @Author nilzxq
+ * @Date 2020-06-28 22:25
+ */
+public class MaxProfit {
+    public static int maxProfit(int[] prices){
+        int cost=Integer.MIN_VALUE,profit=0;
+        for(int price:prices){
+            cost=Math.min(cost,price);
+            profit=Math.max(profit,price-cost);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] profit={7,1,5,3,6,4};
+        System.out.println(maxProfit(profit));
+    }
+}
