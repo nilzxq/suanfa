935d7010650567e9cbbff387afedda16518ed157
nilzxq <2013358717@qq.com>
2020-11-12 09:57:54 +0800

    三数之和

---
+++ src/leetcode/array/Problem_15_ThreeSum.java
@@ -0,0 +1,46 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 三数之和
+ * @Author nilzxq
+ * @Date 2020-11-11 21:02
+ */
+public class Problem_15_ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums){
+        //先排序
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        for(int k=0;k<nums.length-2;k++){
+            if(nums[k]>0){
+                break;
+            }
+            if(k>0&&nums[k]==nums[k-1]){
+                continue;
+            }
+            int i=k+1,j=nums.length-1;
+            while(i<j){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    while(i<j&&nums[i]==nums[++i]) {
+
+                    }
+                }else if(sum>0){
+                    while(i<j&&nums[j]==nums[--j]) {
+
+                    }
+                }else{
+                    res.add(new ArrayList<>(Arrays.asList(nums[k],nums[i],nums[j])));
+                    while(i<j&&nums[i]==nums[++i]) {
+
+                    }
+                    while(i<j&&nums[j]==nums[--j]){}
+                }
+            }
+        }
+        return res;
+    }
+}
